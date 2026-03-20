4c85bf6af78c85020fb91df4dc63951e294c54e6
Sean Parker <seanrichardparker@gmail.com>
2026-03-19 22:53:38 -0400

    finding elements

---
+++ 3_find_elements/find_elements.js
@@ -0,0 +1,19 @@
+"use strict";
+
+// 1. get table with id "age-table"
+let table = document.getElementById("age-table");
+
+// 2. All label elements in that table
+document.querySelectorAll('#age-table label')
+
+// 3. the first td in that table with the word "Age:"
+table.querySelector('td')
+
+// 4. The for w/the name 'search'
+let form = document.getElementsByName('search')[0];
+
+// or
+// document.querySelector('form[name="search"]')
+
+// 5. first input of that form
+form.querySelector('input')
