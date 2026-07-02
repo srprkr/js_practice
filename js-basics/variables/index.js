dc0b1b411370a16fbf1889f9bb6ad2bdb9d00f76
Sean Parker <seanrprkr@gmail.com>
2026-07-02 13:39:44 -0400

    variables and constants

---
+++ js-basics/variables/index.js
@@ -0,0 +1,13 @@
+
+let name = "Shaws";
+
+console.log(name);
+
+// Can't be reserved keyword
+// Should be meaningful
+// Can't start with a number
+// Cannot contain space or hyphen
+
+let firstName = "Sean"; 
+let lastName = "Parker";
+console.log(firstName, lastName);
