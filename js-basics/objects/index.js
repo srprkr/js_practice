b685b4cfe6879ed0ed88bf846248c8bb9e6ebcf6
Sean Parker <seanrprkr@gmail.com>
2026-07-02 20:02:06 -0400

    objects

---
+++ js-basics/objects/index.js
@@ -0,0 +1,16 @@
+
+// Object literal
+let person = {
+  name: 'Sean',
+  age: 39
+};
+
+// Dot notation
+person.name = 'John';
+
+// Bracket Notation
+let selection = 'name';
+// person['name'] = 'Mary';
+person[selection] = 'Mary';
+
+console.log(person.name);
