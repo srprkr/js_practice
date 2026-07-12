913b2633ebc77387529c55141eaf22c25a546811
Sean Parker <seanrprkr@gmail.com>
2026-07-11 23:59:40 -0400

    rectangle object

---
+++ ten_days_of_js/day_4_rectangle_object.js
@@ -0,0 +1,15 @@
+/*
+  length,
+  width,
+  perimeter,
+  value
+*/
+
+function Rectangle(a, b) {
+    return {
+        length: a,
+        width: b,
+        perimeter: 2 * (a + b),
+        area: a * b
+    };
+}
