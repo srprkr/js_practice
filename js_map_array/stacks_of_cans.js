784247d206194180f54f71d44cc20f0260edb020
Sean Parker <seanrprkr@gmail.com>
2026-07-14 00:13:01 -0400

    localizing logic and polishing exit logic

--- js_map_array/stacks_of_cans.js
+++ js_map_array/stacks_of_cans.js
@@ -2,33 +2,27 @@ console.log("StAcKs oF CaNs!!!");
 
 // Count how many steps it takes to reduce a pile to the second highest stack but removing the difference from the highest stack.
 
-let input = [5, 1, 2];
-
+let input = [5, 5, 1, 2];
+let secondHighest;
+let lowestHeight;
 
 let adjustSecondHeight = arr => {
-  
   // uniq cols
   let uniqueHeightsSet = new Set(arr);
-
-  console.log('uniqHts: ', uniqueHeightsSet);
-
-  // track count
-  // update input arr for next round
 
   // get unique heights
   let uniqueHeightsArr = [...uniqueHeightsSet];
   uniqueHeightsArr.sort((a, b) => b - a)
 
   // get second highest stack
-  let secondHighest = uniqueHeightsArr[1];
+  secondHighest = uniqueHeightsArr[1];
   console.log(secondHighest);
 
+  // get lowest stack (for stopping).
+  lowestHeight = uniqueHeightsArr[uniqueHeightsArr.length - 1];
 }
 
 adjustSecondHeight(input);
-
-// get lowest stack (for stopping).
-let lowestHeight = uniqueHeightsArr[uniqueHeightsArr.length - 1];
 console.log("Lowest Ht: ", lowestHeight);
 
 let changeCount = 0;
@@ -36,27 +30,19 @@ let changeCount = 0;
 let equalize_heights = (arr) => {
   arr.map((value, index) => {
 
-    // count cols above secondHighest. Add to count.
+  // count cols above secondHighest. Add to count.
+    // track count - update input arr for next round
     if (value > secondHighest) {
       changeCount += 1;
       arr[index] = secondHighest;
     }     
-  })
-}
+  });
+};
 
-
-
-while (secondHighest > lowestHeight) {
+while (Math.max(...input) > lowestHeight) {
   equalize_heights(input);
   adjustSecondHeight(input);
 }
 
 console.log("inpUT! ", input);
 console.log("changeCount: ", changeCount);
-
-
-
-
-
-
-// output should be 3
